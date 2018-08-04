71b67ba2756c3b828dc65bfb5debf11b082ca96c
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 17:35:21 +0530

    Update TestTriangle.java

--- TestTriangle.java
+++ TestTriangle.java
@@ -4,13 +4,15 @@ import java.util.*;
 public class TriangleTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number of triangles");
+		System.out.println("Enter number of triangles");		//Taking input for no of triangles
 		int num = sc.nextInt();
 		Triangle[] triangle = new Triangle[num];
 		for(int i=0;i<num;i++) {
 			triangle[i] = new Triangle();
+			
+			//Taking input for sides of triangle and storing it in array
 			System.out.println("Enter length of Side 1");
 			double side1 = sc.nextDouble();
 			System.out.println("Enter length of Side 2");
